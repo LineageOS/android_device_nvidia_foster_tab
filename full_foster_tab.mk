a2043422320f5c4ca839faa8bb62fb5415ef6187
Aaron Kling <webgeek1234@gmail.com>
2024-03-25 20:44:34 -0500

    Set build time shipping api level
    The reference tree had to start setting this, requiring it to be
    replicated to all derivatives.
    
    Change-Id: I3a0ca0c5292d18127ca0f93635a64d5e7dd511be

--- full_foster_tab.mk
+++ full_foster_tab.mk
@@ -24,6 +24,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 # Inherit from foster device
 $(call inherit-product, device/nvidia/foster_tab/device.mk)
 
